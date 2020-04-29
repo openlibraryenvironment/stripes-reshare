96b58e28ab4a5b38ddc92429a675a200af6239ea
Jason Skomorowski <jason@indexdata.com>
2020-04-28 20:29:39 -0400

    Move withOkapiKy.js to stripes-reshare

--- index.js
+++ index.js
@@ -12,3 +12,4 @@ export { default as onCloseDirect } from './src/DirectLink/onCloseDirect';
 
 export * as inventoryTypeIds from './src/inventoryTypeIds';
 export { default as useOkapiKy } from './src/okapiKy/useOkapiKy';
+export { default as withOkapiKy } from './src/okapiKy/withOkapiKy';
