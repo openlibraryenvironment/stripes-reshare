bc7ad4edf07a001b83410c7d64e34905ee4790ef
Mike Taylor <mike@indexdata.com>
2020-06-03 11:11:20 +0100

    Fix propType of "component" in <DirectLink>
    This was given as PropTypes.element, but it seems that component
    classes such as stripes-components' `<Button>` are of type
    PropTypes.object.
    
    Fixes a big ugly JS console error.

--- src/DirectLink/DirectLink.js
+++ src/DirectLink/DirectLink.js
@@ -29,7 +29,7 @@ const DirectLink = ({ children, component, to, ...rest }) => {
 };
 
 DirectLink.propTypes = {
-  component: PropTypes.element,
+  component: PropTypes.object,
   to: PropTypes.oneOfType([
     PropTypes.object,
     PropTypes.string
