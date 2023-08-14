2c293dd2629fbe4ee79cca8bf2b7dd799a95f0ad
Jason Skomorowski <jason@indexdata.com>
2023-08-14 18:56:53 -0400

    Add default timeout of 8s to useIntlCallout PR-1513

--- src/useIntlCallout.js
+++ src/useIntlCallout.js
@@ -1,21 +1,25 @@
 /**
  * useIntlCallout: A hook that returns a function:
- *   setMessage(translationKey, type, values, valuesToTranslate)
+ *   setMessage(translationKey, type, values, valuesToTranslate, timeout)
  *
  * ...where translationKey is the message and valuesToTranslate indicate which
  * properties of values are in fact translation keys that must first be translated
  * before passing into formatMessage.
+ * 
+ * Timeout is in ms.
  */
 
 import React, { useContext } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { CalloutContext } from '@folio/stripes/core';
 
+const DEFAULT_TIMEOUT = 8000;
+
 export default () => {
   const callout = useContext(CalloutContext);
   const intl = useIntl();
 
-  return (key, type, values, valuesToTranslate) => {
+  return (key, type, values, valuesToTranslate, timeout = DEFAULT_TIMEOUT) => {
     let intlValues;
     if (values) {
       intlValues = Object.fromEntries(Object.entries(values).map(ent => {
@@ -25,6 +29,6 @@ export default () => {
         return ent;
       }));
     }
-    callout.sendCallout({ message: <FormattedMessage id={key} values={intlValues} />, type });
+    return callout.sendCallout({ message: <FormattedMessage id={key} values={intlValues} />, type, timeout });
   };
 };
