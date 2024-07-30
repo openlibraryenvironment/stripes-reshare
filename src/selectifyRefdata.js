badfa59b6d7ace98f6fc6c961def3e347fe1e9c0
Jason Skomorowski <jason@indexdata.com>
2024-07-30 18:59:00 -0400

    Sort by label when selectifying refdata

--- src/selectifyRefdata.js
+++ src/selectifyRefdata.js
@@ -1,10 +1,12 @@
-// Takes the refdata as it comes from the endpoint and teases out an
-// object to feed to a select control eg. the value property is set to the id.
+// Takes the refdata as it comes from the endpoint and teases out a sorted
+// array of objects to feed to a select control eg. the value property is set
+// to the id.
 const selectifyRefdata = (refdataRecords) => (
   refdataRecords
-    .map(obj => (
+    ?.map(obj => (
       obj.values.map(entry => ({ label: entry.label, value: entry.id }))
     ))[0]
+    ?.sort((a, b) => a.label?.localeCompare(b.label))
 );
 
 export default selectifyRefdata;
