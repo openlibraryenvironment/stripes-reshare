a8143e9e14c618db70099e965ed9de129440d68e
skomorokh <skomorokh@users.noreply.github.com>
2025-07-10 17:12:17 -0400

    useSetting() hook (#28)
    There aren't so many settings that it makes sense to keep requesting
    them separately. Now that we're using them extensively across multiple
    components let's request them all once and be done with it.

---
+++ src/useSettings.js
@@ -0,0 +1,19 @@
+import { useOkapiQuery } from './useOkapiQuery';
+
+/**
+ * A useOkapiQuery for all the settings
+ */
+const useSettings = (key, whole=false) => {
+  return useOkapiQuery('rs/settings/appSettings', {
+    searchParams: {
+      // the presenece of a filter containing "hidden" includes hidden settings
+      // and by not actually filtering we get all the other ones too
+      filters: 'hidden',
+      perPage: '1000',
+    },
+    cacheTime: 24 * 60 * 60 * 1000,
+    staleTime: 2 * 60 * 60 * 1000
+  });
+};
+
+export default useSettings;
